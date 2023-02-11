20fb1b4ecf2da90aa3514450d10e746d94c86754
Gabriel Brasil Melo de Oliveira <uece.gabrielbmo@gmail.com>
2023-02-11 14:09:06 -0300

    Update main.cpp

--- estudo_POO/main.cpp
+++ estudo_POO/main.cpp
@@ -7,7 +7,7 @@ int main(){
     Conta conta_de_gabriel;
     conta_de_gabriel.set_valor(450);
     cout << conta_de_gabriel.get_valor() << endl;
-    conta_de_gabriel.set_nome("VAITOMA NO CU PORRRAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+    conta_de_gabriel.set_nome("Conta de Gabriel.");
     cout << conta_de_gabriel.get_nome() << endl;
     return 0;
 }
